d69b590984f14856fa7bed76293daabf28580ff0
Bhargav <128204030+Bhargav5427@users.noreply.github.com>
2023-08-25 11:22:27 +0530

    Add files via upload

---
+++ right-to-rotate.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main() {
+    int arr[SIZE] = {1, 2, 3, 4, 5};
+    int temp = arr[SIZE - 1];
+    int i;
+    printf("-: RIGHT ROTATED ARRAY :- \n");
+    
+    for (i = SIZE - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = temp;
+
+    printf("Right rotated array: ");
+    for (i = 0; i < SIZE; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+ 
+}
